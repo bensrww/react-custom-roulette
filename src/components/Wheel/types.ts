8e33812d403e23f1fd753e20488c6e53a16a35aa
Ben Chong <bensrww@gmail.com>
2022-03-31 12:26:12 +0800

    add Wheel interface

--- src/components/Wheel/types.ts
+++ src/components/Wheel/types.ts
@@ -7,3 +7,23 @@ export interface StyleType {
   backgroundColor?: string;
   textColor?: string;
 }
+
+export interface Wheel {
+  mustStartSpinning: boolean;
+  prizeNumber: number;
+  data: WheelData[];
+  onStopSpinning?: () => any;
+  backgroundColors?: string[];
+  textColors?: string[];
+  outerBorderColor?: string;
+  outerBorderWidth?: number;
+  innerRadius?: number;
+  innerBorderColor?: string;
+  innerBorderWidth?: number;
+  radiusLineColor?: string;
+  radiusLineWidth?: number;
+  fontSize?: number;
+  perpendicularText?: boolean;
+  textDistance?: number;
+  spinDuration?: number;
+}
